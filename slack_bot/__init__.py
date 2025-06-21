c19a6d511890cb7797f417a2cd2763655247a118
Istvan Olah <stepphan.h@gmail.com>
2025-06-21 03:55:44 +0300

    test meet

--- slack_bot/__init__.py
+++ slack_bot/__init__.py
@@ -8,6 +8,9 @@ def create_app() -> FastAPI:
     from slack_bot.api.slack import slack_router
     app.include_router(slack_router, tags=["slack"])
 
+    from slack_bot.api.fireflies import fireflies_router
+    app.include_router(fireflies_router, tags=["fireflies"])
+
 
     app.add_middleware(
         CORSMiddleware,
